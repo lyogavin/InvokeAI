d5d7338dc9dd6ea578ebc7c177b40503ea003947
Yu Li <lyo.gavin@gmail.com>
2022-11-14 18:48:53 -0600

    fix rename dir

--- run_ocr_title_to_image.sh
+++ run_ocr_title_to_image.sh
@@ -20,12 +20,23 @@ else
   echo ${ROOT_DIR} exist!!!!!!!!!!!!!!!
 fi
 
+ROOT_LOGS_DIR=/home/ubuntu/cloudfs/saved_models/stable_diffusion/finetune_redbook_ocr/logs
+
+if [ ! -d ${ROOT_LOGS_DIR} ];then
+  mkdir -p ${ROOT_LOGS_DIR}
+  echo ${ROOT_LOGS_DIR} created!!!!!!!!!!!!!!
+else
+  echo ${ROOT_LOGS_DIR} exist!!!!!!!!!!!!!!!
+fi
+
+
+
 NAME=finetune_ocr_title_to_image_$run_ts
 
 python3 ./main.py -t \
     --base ./configs/stable-diffusion/v1-finetune_ocr_title_to_image.yaml \
     --actual_resume ./models/ldm/stable-diffusion-v1/v1-5-pruned-emaonly.ckpt \
-    --logdir $ROOT_DIR/logs \
+    --logdir $ROOT_LOGS_DIR \
     -n $NAME \
     --gpus 0,1,2,3,4,5,6,7 \
     --data_root $ROOT_DIR \
